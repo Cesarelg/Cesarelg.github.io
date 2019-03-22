cc1cd6f587c8652995c4b4873f78a38e68e2a2e7
Cesarelg <407596719@qq.com>
2019-03-22 18:02:00 +0800

    xxx

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 cd \d E:/blog/Cesarelg
-git add . && git commmit -m "xxx" && git push origin hexo && hexo clean && hexo g && hexo d
+git add . && git commit -m "xxx" && git push origin hexo && hexo clean && hexo g && hexo d
 pause
